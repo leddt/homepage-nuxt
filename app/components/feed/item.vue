febc09085745a3bece909cf5f76d3662f79ebdf8
David Thibault <david.thibault@sigmund.ca>
2024-11-05 22:15:02 -0500

    Better keyboard handling

--- app/components/feed/item.vue
+++ app/components/feed/item.vue
@@ -14,11 +14,23 @@ const store = useStore()
 
 const isHighlighted = computed(() => store.highlightedItem?.link === props.item.link)
 const highlightedElement = computed(() => isHighlighted.value ? store.highlightedElement : null)
+
+const openLink = (item: FeedItem) => {
+  store.setHighlightedItem(item, 'link')
+  window.open(item.link, '_blank')
+}
 </script>
 
 <template>
-  <li class="group transition-all sm:hover:bg-muted/50 px-2 py-1 rounded-lg" :class="{ 'outline outline-1 outline-blue-500': isHighlighted }">
+  <li
+    tabindex="0"
+    class="group transition-colors sm:hover:bg-muted/50 px-2 py-1 rounded-lg"
+    :class="{ 'outline outline-1 focus:outline-2 outline-blue-500': isHighlighted }"
+    @keydown.self.enter="openLink(item)"
+    @keydown.self.space="openLink(item)"
+  >
     <NuxtLink
+      tabindex="-1"
       :href="item.link"
       target="_blank"
       class="text-lg hover:underline block"
@@ -36,7 +48,7 @@ const highlightedElement = computed(() => isHighlighted.value ? store.highlighte
         v-if="item.comments"
         :href="item.comments"
         target="_blank"
-        class="flex items-center hover:underline hover:text-primary sm:opacity-0 group-hover:opacity-100 transition-opacity"
+        class="flex items-center hover:underline hover:text-primary sm:opacity-0 group-hover:opacity-100 group-focus:opacity-100 focus:opacity-100 transition-opacity"
         :class="{ 'underline sm:opacity-100': highlightedElement === 'comments' }"
         @mouseup="store.setHighlightedItem(item, 'comments')"
       >

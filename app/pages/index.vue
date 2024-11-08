9dd2f9ab34ea0908b95453b84b2533bd25658626
David Thibault <david.thibault@sigmund.ca>
2024-11-08 03:02:03 -0500

    magic key instead of keydown to clear highlighted item

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,10 +10,13 @@ function onEscape() {
   (document.activeElement as HTMLElement)?.blur()
   clearHighlightedItem()
 }
+
+const { escape } = useMagicKeys()
+whenever(escape!, onEscape)
 </script>
 
 <template>
-  <div class="container mx-auto p-4 max-w-7xl" @keydown.esc="onEscape">
+  <div class="container mx-auto p-4 max-w-7xl">
     <div class="grid md:grid-cols-2 gap-4 lg:gap-8">
       <Feed v-for="(feed, index) in feeds" :key="index" :feed />
     </div>

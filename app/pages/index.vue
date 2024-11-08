7d31b1c0a557681ecbf072988333a02c300ba3e8
David Thibault <david.thibault@sigmund.ca>
2024-11-08 02:02:54 -0500

    Clear hightlighted item on escape

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,10 +3,17 @@ const feeds = [
   useFeed('hn', 'Hacker News', 'ion:logo-hackernews', 'text-orange-500'),
   useFeed('sd', 'Slashdot', 'simple-icons:slashdot', 'text-emerald-500'),
 ]
+
+const { clearHighlightedItem } = useStore()
+
+function onEscape() {
+  (document.activeElement as HTMLElement)?.blur()
+  clearHighlightedItem()
+}
 </script>
 
 <template>
-  <div class="container mx-auto p-4 max-w-7xl">
+  <div class="container mx-auto p-4 max-w-7xl" @keydown.esc="onEscape">
     <div class="grid md:grid-cols-2 gap-4 lg:gap-8">
       <Feed v-for="(feed, index) in feeds" :key="index" :feed />
     </div>

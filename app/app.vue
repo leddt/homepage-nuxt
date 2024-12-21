79ff4badce693e15347d8209e3a419dc3b99df1b
David Thibault <david.thibault@sigmund.ca>
2024-12-20 21:29:58 -0500

    Add background gradient

--- app/app.vue
+++ app/app.vue
@@ -8,7 +8,8 @@ useHead({
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
 
-  <div class="bg-slate-200 min-h-screen">
+  <div class="z-0 fixed top-0 left-0 right-0 bottom-0 bg-gradient-to-br from-slate-200 via-sky-100 to-purple-100" />
+  <div class="z-10 relative">
     <NuxtPage />
   </div>
 </template>

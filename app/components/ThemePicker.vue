03038525c5d7bae19c94db65489c77523315a532
David Thibault <david.thibault@sigmund.ca>
2025-02-23 11:38:02 -0500

    Add padding

--- app/components/ThemePicker.vue
+++ app/components/ThemePicker.vue
@@ -3,7 +3,7 @@ const mode = useColorMode()
 </script>
 
 <template>
-  <div class="fixed bottom-0 right-0 z-10 bg-secondary text-secondary-foreground rounded-tl-lg flex flex-row gap-1 leading-none opacity-50 hover:opacity-100">
+  <div class="fixed bottom-0 right-0 pr-4 z-10 bg-secondary text-secondary-foreground rounded-tl-lg flex flex-row gap-1 leading-none opacity-50 hover:opacity-100">
     <button
       class="p-1"
       :class="{ 'text-green-500': mode.preference === 'light' }"

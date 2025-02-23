2b957eecf533f3062a0c34d1640c1e33f78f1ebf
David Thibault <david.thibault@sigmund.ca>
2025-02-23 11:34:34 -0500

    Add theme picker

--- app/app.vue
+++ app/app.vue
@@ -12,4 +12,5 @@ useHead({
   <div class="z-10 relative">
     <NuxtPage />
   </div>
+  <ThemePicker />
 </template>
---
+++ app/components/ThemePicker.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const mode = useColorMode()
+</script>
+
+<template>
+  <div class="fixed bottom-0 right-0 z-10 bg-secondary text-secondary-foreground rounded-tl-lg flex flex-row gap-1 leading-none opacity-50 hover:opacity-100">
+    <button
+      class="p-1"
+      :class="{ 'text-green-500': mode.preference === 'light' }"
+      title="Use light theme"
+      @click="mode.preference = 'light'"
+    >
+      <Icon name="mdi:weather-sunny" />
+    </button>
+    <button
+      class="p-1"
+      :class="{ 'text-green-500': mode.preference === 'dark' }"
+      title="Use dark theme"
+      @click="mode.preference = 'dark'"
+    >
+      <Icon name="mdi:weather-night" />
+    </button>
+    <button
+      class="p-1"
+      :class="{ 'text-green-500': mode.preference === 'system' }"
+      title="Follow system settings"
+      @click="mode.preference = 'system'"
+    >
+      <Icon name="mdi:monitor" />
+    </button>
+  </div>
+</template>
